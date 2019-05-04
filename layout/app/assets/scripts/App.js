8f1ea98d8a19d8c39c24fe7f8a8d776e30143a4a
Anatolii <anatolii.lukavenko@gmail.com>
2019-05-04 17:41:09 +0300

    Dev Environment: favicon, fonts

---
+++ layout/app/assets/scripts/App.js
@@ -0,0 +1,20 @@
+// 3rd party packages from NPM
+import $ from 'jquery'; 
+// import $ from 'jquery'; 
+
+// Our modules / classes 
+import CopyrightYear 				from './modules/copyright-year';
+import UIToTop 							from './modules/ui-to-top';
+import Wow			  					from './modules/wow';
+import Parallax 						from './modules/material-parallax';
+
+// Instantiate a new object using our modules/classes
+var copyrightYear 			= new CopyrightYear();
+var uitotop 						= new UIToTop();
+var wow 								= new Wow();
+var paralax 						= new Parallax();
+
+
+
+
+
